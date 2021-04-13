deffb9365205e863185ac9938e774c65ec62ffe4
Andres Pelaez <andres.pelaezf@hotmail.com>
2021-04-12 21:06:56 -0500

    LoginPage to functional component

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,59 +1,43 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { setAuthedUser } from '../actions/authedUser'
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setAuthedUser } from '../actions/authedUser';
 
-class LoginPage extends Component {
-	state = {
-		user: '',
-	}
-	selectUser = (e) => {
-		const user = e.target.value
+const LoginPage = () => {
+  const [selectedUser, setSelectedUser] = useState(true);
+  const users = useSelector((state) => state.users);
 
-		this.setState(() => ({
-			user,
-		}))
-	}
+  const submitSelect = () => {
+    const dispatch = useDispatch();
+    dispatch(setAuthedUser(selectedUser));
+  };
 
-	submitSelect = () => {
-		const { dispatch } = this.props
-		const { user } = this.state
+  return (
+    <div className="login-page">
+      <h2>Login Page</h2>
+      <p>Select a user to login with:</p>
+      <select
+        onChange={(e) => {
+          setSelectedUser(e.target.value);
+        }}
+        defaultValue="0"
+      >
+        <option value="0" disabled>
+          Choose a user
+        </option>
+        {Object.keys(users).map((userId) => {
+          const user = users[userId];
+          return (
+            <option key={user.id} value={user.id}>
+              {user.name}
+            </option>
+          );
+        })}
+      </select>
+      <button type="button" className="btn" onClick={submitSelect} disabled={selectedUser === ''}>
+        Login
+      </button>
+    </div>
+  );
+};
 
-		dispatch(setAuthedUser(user))
-	}
-	render() {
-		const { users } = this.props
-		const { user } = this.state
-		return (
-			<div className='login-page'>
-				<h2>Login Page</h2>
-				<p>Select a user to login with:</p>
-				<select onChange={this.selectUser} defaultValue='0'>
-					<option value='0' disabled>
-						Choose a user
-					</option>
-					{Object.keys(users).map((userId) => {
-						const user = users[userId]
-						return (
-							<option key={user.id} value={user.id}>
-								{user.name}
-							</option>
-						)
-					})}
-				</select>
-				<button
-					className='btn'
-					onClick={this.submitSelect}
-					disabled={user === '' ? true : false}
-				>
-					Login
-				</button>
-			</div>
-		)
-	}
-}
-
-function mapStateToProps({ users }) {
-	return { users }
-}
-
-export default connect(mapStateToProps)(LoginPage)
+export default LoginPage;
